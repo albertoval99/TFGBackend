a238abcdb8ee4e1cf8114d67e9cc29c59d83a784
albertoval99 <albertovalabad@gmail.com>
2025-05-19 15:44:39 +0200

    Estadisticas de cada jugador OK

--- src/estadisticas/domain/EstadisticasTotales.ts
+++ src/estadisticas/domain/EstadisticasTotales.ts
@@ -2,6 +2,7 @@ export interface EstadisticasTotales {
     id_jugador: number;
     nombre: string;
     apellidos: string;
+    dorsal: number;
     goles: number;
     tarjetas_amarillas: number;
     tarjetas_rojas: number;
