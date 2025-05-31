234cdecefc7a00631a452ca2ee046bd29870af30
albertoval99 <albertovalabad@gmail.com>
2025-05-31 14:41:07 +0200

    puertos

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const port = 3000; //Para localhost y despliegue este
 app.listen( port,  () => {
   console.log('🚀 Servidor corriendo en:');
   console.log(`- Local: http://localhost:${port}`);
-  console.log(`- Red: http://192.168.20.134:${port}`);
+  console.log(`- Red: http://192.168.20.134:${port}`); //apache
 });
 /**
  * app.listen(port, () => {
